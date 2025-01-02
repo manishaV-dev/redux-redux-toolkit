76a515040df29debd422f36a9475b8b86b604181
Manisha varma <manishavkapex1@gmail.com>
2025-01-02 16:39:22 +0530

    creating two reducers

--- 01-Basic/multiple-reducer.js
+++ 01-Basic/multiple-reducer.js
@@ -17,19 +17,29 @@ function orderBurger() {
   };
 }
 
-
-const initialState = {
+const initialStateForPizza = {
   pizzaBase: 100,
+};
+
+const initialStateForBurger = {
   burgerBuns: 200,
 };
 
-const reducer = (state = initialState, action) => {
+const reducerForPizza = (state = initialStateForPizza, action) => {
   switch (action.type) {
     case ORDER_PIZZA:
       return {
         ...state,
         pizzaBase: state.pizzaBase - 1,
       };
+    default: {
+      return state;
+    }
+  }
+};
+
+const reducerForBurger = (state = initialStateForBurger, action) => {
+  switch (action.type) {
     case ORDER_BURGER:
       return {
         ...state,
