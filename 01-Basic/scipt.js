5edc403e0ade848059e0dada654c96d8a024ea1c
Manisha varma <manishavkapex1@gmail.com>
2025-01-02 13:50:21 +0530

    about initialState, action and reducer function

--- 01-Basic/scipt.js
+++ 01-Basic/scipt.js
@@ -1,1 +1,34 @@
-console.log("Welcome to redux");
+const ORDER_PIZZA = "Pizza Order";
+
+// Action
+// const action = {
+//   type: ORDER_PIZZA,
+//   shop_name: "Pizza House",
+// };
+
+// Action Creator --- a function that return action
+function orderPizza() {
+  return {
+    type: ORDER_PIZZA,
+    shop_name: "Pizza House",
+  };
+}
+
+// --------------------- Reducer
+const initialState = {
+  pizzaBase: 100,
+  toppings: ["Cheese", "Olive"],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ORDER_PIZZA:
+      return {
+        ...state,
+        pizzaBase: pizzaBase - 1,
+      };
+    default: {
+      return state;
+    }
+  }
+};
