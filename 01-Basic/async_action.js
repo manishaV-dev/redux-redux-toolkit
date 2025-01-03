5ce916881e5aa0a892f30e72064519fb5964eaeb
Manisha varma <manishavkapex1@gmail.com>
2025-01-03 12:54:51 +0530

    apply redux thunk middleware

--- 01-Basic/async_action.js
+++ 01-Basic/async_action.js
@@ -1,6 +1,11 @@
 const redux = require("redux");
 const createStore = redux.createStore;
 
+const applyMiddleWare = redux.applyMiddleware;
+const thunk = require("redux-thunk").thunk;
+
+const axios = require("axios");
+
 // 2-  action
 const FETCH_REQUEST = "FETCH_REQUEST";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
@@ -62,9 +67,39 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+// 6. Thunk Action creator - this action creator return a function (does not have to be pure function whuch means we call define asynchronousapp call and make side effects) unlike other action creator and also we can dispatch an action inside this function
+
+const fetchProducts = () => {
+  return function (dispatch) {
+    dispatch(fetchRequest());
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        // const products = res.data
+        // console.log(products);
+
+        const products = res.data.map((product) => product.title);
+        // console.log(products);
+        dispatch(fetchSuccess(products))
+        
+      })
+      .catch((error) => {
+        dispatch(fetchError());
+      });
+  };
+};
 
 // 5. Create store
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleWare(thunk));
 
+// 7
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(fetchProducts());
 
 // Thunk allows redux to wait before dispatch fetch data inside the store
+
+// To use async API call in redux we need two package
+// 1. axios - Make get request to an API endpoint
+
+// 2. Redux-Thunk - Using redux thank, we can define an action creator with network request using which we can make api call
