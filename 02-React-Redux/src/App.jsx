7d1fa55d38dc3dc0e9bc21a35fbbb774333e59ba
Manisha varma <manishavkapex1@gmail.com>
2025-01-03 14:49:29 +0530

    create a component and add styling

--- 02-React-Redux/src/App.jsx
+++ 02-React-Redux/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Pizzabox from './components/Pizzabox'
 
 function App() {
 
   return (
     <>
       <h1>React-Redux</h1>
+      <Pizzabox />
     </>
   )
 }
---
+++ 02-React-Redux/src/components/Pizzabox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pizzabox() {
+  return (
+    <>
+    <div className="container">
+        <h2 className='text'>Number of Pizza Base available - 100</h2>
+        <button className='btn'>Order Pizza</button>
+    </div>
+    </>
+  )
+}
+
+export default Pizzabox
