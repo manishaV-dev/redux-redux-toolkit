f30400f5f918691b457341605902f1d7809398a9
Manisha varma <manishavkapex1@gmail.com>
2025-01-15 13:55:59 +0530

    dispatch an action

--- 04-React-Redux-Toolkit/src/features/burger/BurgerView.jsx
+++ 04-React-Redux-Toolkit/src/features/burger/BurgerView.jsx
@@ -1,12 +1,16 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { burger_order } from "./burgerSlice";
 
 function BurgerView() {
+  const burgerBuns = useSelector((state) => state.burger.burgerBuns);
+  const dispatch = useDispatch()
+
   return (
     <>
-      <h2>Number os Burger Buns : </h2>
-      <button>Order Burger</button>
+      <h2>Number os Burger Buns : {burgerBuns} </h2>
+      <button onClick={() => dispatch(burger_order())}>Order Burger</button>
     </>
-  )
+  );
 }
 
-export default BurgerView
+export default BurgerView;
--- 04-React-Redux-Toolkit/src/features/pizza/PizzaComponent.jsx
+++ 04-React-Redux-Toolkit/src/features/pizza/PizzaComponent.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { pizza_order } from "./pizzaSlice";
 
 function PizzaComponent() {
+  //   const pizzaBase = useSelector((state) => console.log(state));
+  // --- console-log burger: {burgerBuns: 200}
+  // pizza: {pizzaBase: 1000}
+
+  const pizzaBase = useSelector((state) => state.pizza.pizzaBase);
+  const dispatch = useDispatch();
+
   return (
     <>
-      <h2>Number os Pizza Base : </h2>
-      <button>Order Pizza</button>
+      <h2>Number os Pizza Base : {pizzaBase} </h2>
+      <button onClick={() => dispatch(pizza_order())}>Order Pizza</button>
     </>
-  )
+  );
 }
 
-export default PizzaComponent
+export default PizzaComponent;
