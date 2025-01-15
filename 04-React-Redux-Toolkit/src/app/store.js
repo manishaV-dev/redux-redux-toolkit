5302a85a33be848ef0489a94162f0d4ccf31ac82
Manisha varma <manishavkapex1@gmail.com>
2025-01-15 13:23:19 +0530

    configure store

---
+++ 04-React-Redux-Toolkit/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import pizzaReducer from "../features/pizza/pizzaSlice";
+import burgerReducer from "../features/burger/burgerSlice";
+
+const store = configureStore({
+  reducer: {
+    pizza: pizzaReducer,
+    burger: burgerReducer,
+  },
+});
+
+export default store;
