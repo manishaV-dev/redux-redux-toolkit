f669e49e9820bfb31282fcb8fafd2cd3ea417f94
Manisha varma <manishavkapex1@gmail.com>
2025-02-01 16:38:34 +0530

    add sup

--- 07-Shopping-Cart/src/components/Navbar.jsx
+++ 07-Shopping-Cart/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { items } = useSelector((state) => state.cart);
+
   return (
     <>
       <nav>
         <h1>Shopping Cart</h1>
         <div>
           <Link to={"/"}>Home</Link>
-          <Link to={"/cart"}>Cart</Link>
+          <Link to={"/cart"}>
+            Cart <sup>{items.length}</sup>{" "}
+          </Link>
         </div>
       </nav>
     </>
